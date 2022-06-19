a2bf2302530e9716e049f78b8d32505aab0b5e3e
Kelvin <vokemalosh32@gmail.com>
2022-06-19 17:15:04 +0300

    commit tree 1c42a4a26cf63a8be86613ad361453b3a37f1cc3

--- main.js
+++ main.js
@@ -3,10 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector("#modal");
+modal.classList.add("hidden");
 
-
-
-
+ const clickLike= (event)=>{
+  const heart = evemt.target
+  heart.innerText = EMPTY_HEART
+  mimicServerCall()
+    .then(res =>{
+      if(heart.innerText===EMPTY_HEART){
+      heart.innerText =FULL_HEART
+      heart.classList.add("activated-heart")
+    }
+    else{
+      heart.innerText =EMPTY_HEART
+      heart.classList.remove("activated-heart")
+    }
+  })
+    .catch(err=>{
+      modal.classList.remove("hidden")
+      modal.querySelector("#modal-message").textContext =err
+      setTimeout(()=>{
+        modal.classList.add("hidden")
+      }, 3000)
+    })
+ }
+ const hearts = document.querySelectorAll(".like-glyph");
+  hearts.forEach( heart => {
+  heart.addEventListener("click", click)
+})
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
